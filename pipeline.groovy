75a7f33978e7e534fbf97f9d0556232559b3a06e
Wen Xiu <xwen@thoughtworks.com>
2019-01-24 13:31:47 +0800

    removed the useless code and renamed pipeline stages

--- pipeline.groovy
+++ pipeline.groovy
@@ -10,36 +10,30 @@ pipeline{
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Wen-Xiu/dddsample.git']]])
             }
         }
-        stage ('mvn test'){
-            //mvn 测试
+        stage ('test'){
             steps {
                 sh "mvn test"
             }
         }
 
-        stage ('mvn build'){
-            //mvn构建
+        stage ('build'){
             steps {
                 sh "mvn clean install -Dmaven.test.skip=true"
             }
         }
 
-        stage ('publish') {
+        stage ('push') {
             steps {
                 sh '''
-                  pip install awscli
-                  echo "Not logged into ECR yet, logging in"
-                  login_command=$(aws ecr get-login --registry-ids 494526681395 --region ap-southeast-1--no-include-email)
-                  eval "${login_command}" &> /dev/null
-                  if docker pull 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/dddsample; then
-                  echo "Login succeeded"
-                  else
-                  echo "login failed"
-                  fi
-                  
                   docker build . -f Dockerfile -t 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/dddsample
                   docker push 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/dddsample
                 '''
+            }
+        }
+
+        stage ('dev deploy') {
+            steps {
+                sh 'echo deploy to dev environment'
             }
         }
     }
