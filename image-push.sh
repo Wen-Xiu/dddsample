6ca1102086dbd5fb55e3258dce95f2f30103d067
Wen Xiu <xwen@thoughtworks.com>
2019-01-24 17:00:52 +0800

    push image to ecr

---
+++ image-push.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+docker images
+    apk -v --update add \
+        python \
+        py-pip \
+        groff \
+        less \
+        mailcap \
+        && \
+    pip install --upgrade awscli==1.14.5 s3cmd==2.0.1 python-magic && \
+    apk -v --purge del py-pip && \
+    rm /var/cache/apk/*
+
+docker build -f Dockerfile -t 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/dddsample .
+
+docker images|grep dddsample
+
+login_command=$(aws ecr get-login --registry-ids 494526681395 --region eu-central-1 --no-include-email)
+
+eval "${login_command} | sed 's|https://||'"
+
+docker push 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/dddsample
--- pipeline.groovy
+++ pipeline.groovy
@@ -24,7 +24,7 @@ pipeline{
 
         stage ('push') {
             steps {
-                sh 'echo WIP: push image to repository'
+                sh './image-push.sh'
             }
         }
 
